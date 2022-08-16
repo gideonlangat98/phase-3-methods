5f8b8874e6018439e460a0eca084376c01b97bac
gideonlangat98 <gideonlangat98@gmail.com>
2022-08-16 17:22:53 +0300

    Done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+#without argument
+def greet_programmer()
+    puts ("Hello, programmer!")
+end
+greet_programmer()
+
+#with argument
+def greet(name = "Naureen")
+    puts ("Hello, #{name}!")
+end
+greet
+
+#with default
+def greet_with_default(name = "programmer")
+    puts ("Hello, #{name}!")
+end
+greet_with_default
+
+#sum of two numbers
+def add(num1, num2)
+    return num1 + num2
+end
+
+#return half of integer
+def halve(number = "6")
+    if number.to_i === 6
+        return number / 2
+    else
+        return nil
+    end
+end
+    
